c626821e2df733148ca98f082442195bc148ddc3
narwa <narwandaru.nara@gmail.com>
2024-04-13 21:13:59 +0700

    refactor: removed 'simple' from the application

--- src/main/java/dev/vianneynara/simpleportoapi/PortoApiApplication.java
+++ src/main/java/dev/vianneynara/simpleportoapi/PortoApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimplePortoApiApplication {
+public class PortoApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimplePortoApiApplication.class, args);
+		SpringApplication.run(PortoApiApplication.class, args);
 	}
 
 }
