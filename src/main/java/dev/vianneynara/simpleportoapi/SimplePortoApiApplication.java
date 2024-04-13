a8495ee86c88ec1198c7f698d4390f74ccc5e879
narwa <narwandaru.nara@gmail.com>
2024-04-13 20:33:45 +0700

    Initial Commit

---
+++ src/main/java/dev/vianneynara/simpleportoapi/SimplePortoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.vianneynara.simpleportoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePortoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePortoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vianneynara/simpleportoapi/SimplePortoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vianneynara.simpleportoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePortoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
