8c0325c61ea422806f91002a2ecd589219d4e93b
narwa <narwandaru.nara@gmail.com>
2024-04-14 22:53:28 +0700

    feat: added ProjectService
    With basic CRUD operations and custom exception.

---
+++ src/main/java/dev/vianneynara/portoapi/service/ProjectService.java
@@ -0,0 +1,15 @@
+package dev.vianneynara.portoapi.service;
+
+import dev.vianneynara.portoapi.model.Project;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public interface ProjectService {
+	Project createProject(Project project);
+	Project retrieveProject(Long id);
+	void updateProject(Long id, Project project);
+	void deleteProject(Long id);
+	Collection<Project> retrieveProjects();
+}
